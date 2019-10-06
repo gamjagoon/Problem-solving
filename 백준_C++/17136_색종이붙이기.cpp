8977d19f905e5530668b7c102145b9c796039975
gamjagoon <gamjagoon@users.noreply.github.com>
2019-10-06 12:04:42 +0900

    asdf

--- 백준_C++/17136_색종이붙이기.cpp
+++ 백준_C++/17136_색종이붙이기.cpp
@@ -1,33 +1,73 @@
 #include <iostream>
 #include <cstdio>
+#include <algorithm>
 
 using namespace std;
 
-int arr[10][10]; //변결할것
+
+int arr[10][10];
 int index = 1;
-int result = 0;
-int cnt[6] = {-1,5,5,5,5,5};
+int cnt,result = 25;
+int paper[6] = {-1,5,5,5,5,5};
 int parent[10][10];
-int count[101];
+int element[101];
+int paper_use[6] = {0,0,0,0,0,0};
 
 void display();
 void make_union(const int y,const int x);
-void calc_rec();
+void remove_square();
 bool remove_rec(const int u,const int n);
 bool view_cnt();
+void find_first_element(int &x,int &y,int u);
+void find_last_element(int &x,int &y,int u);
+bool Inspection(int x,int y,int n,int u);
+void func(int y, int x,int x_end,int y_end,int u);
+void paper_now();
+
 
 int main(){
-	for(int y = 0;y < 10;++y){
-		for(int x = 0;x< 10;++x){
-			scanf("%d",&arr[y][x]);
-			if(arr[y][x] == 1)make_union(y,x);
+	for(int i = 0;i < 10;++i){
+		for(int j = 0;j< 10;++j){
+			scanf("%d",&arr[i][j]);
+			if(arr[i][j] == 1)make_union(i,j);
 		}
 	}
-	calc_rec();
-	if(!view_cnt())return 0;
-	display();
-	int cntcpy[6] = {-1,cnt[1],cnt[2],cnt[3],cnt[4],cnt[5]};
-
+	cnt = 0;
+	remove_square();
+	bool flag = true; 
+	for(int i = 1; i < 6; ++i){
+		if(paper[i] < 0){
+			flag = false;
+			break;
+		}
+	}
+	if(!flag){
+		printf("-1\n");
+		return 0;
+	}
+	for(int i = 1 ; i < index;++i){
+		if(element[i] != 0)
+		{
+			flag = false;
+			break;
+		}
+	}
+	if(flag){
+		result = min(cnt,result);
+		printf("%d",result);
+	}
+	else{
+		for(int i = 1;i < index;++i){
+			int x,y,x_end,y_end;
+			if(element[i] != 0){
+				find_first_element(x,y,i);
+				find_last_element(x_end,y_end,i);
+				func(y,x,x_end,y_end,i);
+			}
+		}
+		printf("%d\n",result);
+	}
+	return 0;
 }
 
 void display(){
@@ -39,9 +79,7 @@ void display(){
 		printf("\n");
 	}
 }
-/*
-[y-1][x] -> [y][x-1] 순서로 탐색한다
-*/
+
 void make_union(const int y,const int x){
 	int key = index;
 	if(y-1>=0 && parent[y-1][x] != 0){
@@ -60,39 +98,37 @@ void make_union(const int y,const int x){
 		}
 	}
 	parent[y][x] = key;
-	arr[y][x] = key;
-	count[key]++;
+	element[key]++;
 	index++;
 }
-
-void calc_rec(){
-	for(int i = 0 ; i < index;++i){
-		int n = count[i];
+void remove_square(){
+	for(int i = 1 ; i < index;++i){
+		int n = element[i];
 		if(n == 0)continue;
 		switch(n){
 			case 1:
 			if(remove_rec(i,1)){
-				count[i] = 0;
+				element[i] = 0;
 			}
 			continue;
 			case 4:
 			if(remove_rec(i,2)){
-				count[i] = 0;
+				element[i] = 0;
 			}
 			continue;
 			case 9:
-			if(remove_rec(i,9)){
-				count[i] = 0;
+			if(remove_rec(i,3)){
+				element[i] = 0;
 			}
 			continue;
 			case 16:
 			if(remove_rec(i,4)){
-				count[i] = 0;
+				element[i] = 0;
 			}
 			continue;
 			case 25:
 			if(remove_rec(i,5)){
-				count[i] = 0;
+				element[i] = 0;
 			}
 			continue;
 			default:
@@ -100,47 +136,133 @@ void calc_rec(){
 		}
 	}
 }
-
-bool remove_rec(const int u,const int n){
-	int idx,idy;
+void find_first_element(int &x,int &y,int u){
 	bool find = false;
 	for(int i = 0 ; i < 10;++i){
 		for(int j = 0 ; j < 10;++j){
 			if(parent[i][j] == u){
-				idx = j;
-				idy = i;
+				x = j;
+				y = i;
 				find = true;
 				break;
 			}
 		}
 		if(find == true)break;
 	}
-	if(parent[idy + n - 1][idx + n -1] != u)return false;
-	if(parent[idy + n - 1][idx] != u)return false;
-	if(parent[idy][idx + n - 1] != u)return false;
-	for(int i = idy;i < idy+n;++i){
-		for(int j = idx;j < idx+n;++j){
+	return;
+}
+void find_last_element(int &x,int &y,int u){
+	bool find = false;
+	for(int i = 9 ; i >=0;--i){
+		for(int j = 9 ; j >= 0;--j){
+			if(parent[i][j] == u){
+				x = j;
+				y = i;
+				find = true;
+				break;
+			}
+		}
+		if(find == true)break;
+	}
+	return;
+}
+bool Inspection(int x,int y,int n,int u){
+	if(parent[y + n - 1][x + n -1] != u)return false;
+	if(parent[y + n - 1][x] != u)return false;
+	if(parent[y][x + n - 1] != u)return false;
+	for(int i = y;i < y+n;++i){
+		for(int j = x;j < x+n;++j){
 			if(parent[i][j] != u){
 				return false;
 			}
 		}
 	}
-	for(int i = idy;i < idy+n;++i){
-		for(int j = idx;j < idx+n;++j){
-			arr[i][j] = 0;
+	return true;
+}
+bool remove_rec(const int u,const int n){
+	int x,y;
+	find_first_element(x,y,u);
+	Inspection(x,y,n,u);
+	for(int i = y;i < y+n;++i){
+		for(int j = x;j < x+n;++j){
 			parent[i][j] = 0;
 		}
 	}
-	cnt[n]-=1;
+	paper[n]-=1;
+	cnt += 1;
 	return true;
 }
 
-bool view_cnt(){
-	for(int i = 1;i < 6 ; ++i){
-		if(cnt[i] < 0 ){
-			printf("-1");
-			return false;
-		}
+int good_n(int u){
+	int loc = element[u];
+	if(loc < 4)return 1;
+	if(loc < 9)return 2;
+	if(loc < 16)return 3;
+	if(loc < 25)return 4;
+	return 5;
+}
+
+/*시작지점과 종료지점 그리고 구하는 유니온을 선언
+결과 배열에 저장*/
+void func(int y, int x,int x_end,int y_end,int u)
+{
+	if (x == x_end+1)
+	{
+		func(y + 1, 0,x_end,y_end,u);
+		return;
 	}
-	return true;
+	if (y == y_end+1)
+	{
+		
+		if(result < cnt){
+			return;
+		}
+		else{
+			cnt
+		}
+		return;
+	}
+	if (parent[y][x] != u)
+	{
+		func(y, x + 1,x_end,y_end,u);
+		return;
+	}
+	int a = good_n(u);
+	// printf("a = %d\n",a);
+	for (int n = a; n >= 1; n--)
+	{
+		if (paper[n] == 0 || y + n > 10 || x + n > 10)
+		{
+			continue;
+		}
+		if(!Inspection(x,y,n,u))continue;
+		for (int j = y; j < y + n; j++)
+		{
+			for (int k = x; k < x + n; k++)
+			{
+				parent[j][k] = 0;
+			}
+		}
+		paper[n]--;
+		paper_use[n]++;
+		cnt++;
+		func(y, x + n,x_end,y_end,u);
+		for (int j = y; j < y + n; j++)
+		{
+			for (int k = x; k < x + n; k++)
+			{
+				parent[j][k] = u;
+			}
+		}
+		paper[n]++;
+		paper_use[n]--;
+		cnt--;
+	}
+}
+
+void paper_now(){
+	for(int i = 1;i < 6;++i){
+		printf("%d ",paper[i]);
+	}
+	printf("\n");
 }
