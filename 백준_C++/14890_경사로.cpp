be0be1c56b3e11451a2d902e547cb340c721e94a
gamjagoon <gamjagoon@users.noreply.github.com>
2019-12-07 15:36:00 +0900

    경사로문제

--- 백준_C++/14890_경사로.cpp
+++ 백준_C++/14890_경사로.cpp
@@ -1,123 +1,79 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
+#define nfor(i,s,e) for(auto i = s; i < e; ++i)
 using namespace std;
-int N, L, result = 0;
-vector<int>col[100];
-vector<int>row[100];
-void search_line(vector<int>&v,int n,int prev,bool up) {
-	if (n >= N)return;
-	else {
-		if (up) {
-			for (int i = n - L; i < n; ++i) {
-				if (v[i] != prev) {
-					return;
-				}
-			}
-			if (v[n] == prev + 1) {
-				if (n == N-1) {
-					result++;
-					return;
-				}
-				search_line(v, n + L, prev + 1, true);
-				search_line(v, n + 1, prev + 1, false);
-			}
-			if (v[n] == prev - 1) {
-				if (n == N-1) {
-					result++;
-					return;
-				}
-				search_line(v, n + L, prev - 1, true);
-				search_line(v, n + 1, prev - 1, false);
-			}
-		}
-		else {
-			if (prev == v[n]) {
-				if (n == N - 1) {
-					result++;
-					return;
-				}
-				search_line(v, n + L, prev, true);
-				search_line(v, n+1, prev, false);
-			}
-			else {
-				return;
-			}
-		}
-	}
-}
-
-void search_line_reverse(vector<int>& v, int n, int prev, bool up) {
-	if (n < 0)return;
-	// 다리를 놓는 경우 
-	else {
-		if (up) {
-			for (int i = n+L; i > n; --i) {
-				if (v[i] != prev) {
-					return;
-				}
-			}
-			if (v[n] == prev + 1) {
-				if (n == 0) {
-					result++;
-					return;
-				}
-				search_line_reverse(v, n - L, prev + 1, true);
-				search_line_reverse(v, n - 1, prev + 1, false);
-			}
-			if (v[n] == prev - 1) {
-				if (n == 0) {
-					result++;
-					return;
-				}
-				search_line_reverse(v, n - L, prev - 1, true);
-				search_line_reverse(v, n - 1, prev - 1, false);
-			}
-		}
-		//다리를 안놓았다
-		else {
-			if (prev == v[n]) {
-				if (n == 0) {
-					result++;
-					return;
-				}
-				search_line_reverse(v, n - L, prev, true);
-				search_line_reverse(v, n - 1, prev, false);
-			}
-			else {
-				return;
-			}
-		}
-	}
-}
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> N >> L;
+int N,L;
+vector<int>col[101];
+vector<int>raw[101];
+bool ch[101]= {false};
+void init()
+{
+	ios::sync_with_stdio(false);cin.tie(0);
+	cin>>N>>L;
 	int tmp;
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
+	nfor(i,0,N)
+	{
+		nfor(j,0,N){
 			cin>>tmp;
 			col[i].push_back(tmp);
-			row[j].push_back(tmp);
+			raw[j].push_back(tmp);
 		}
 	}
-	for (int i = 0; i < col->size(); ++i) {
-		int prev = result;
-		search_line(row[i], 0, row[i][0], false);
-		if (prev != result)printf("row %d prev\n", i);
-		if (prev == result) {
-			search_line_reverse(row[i], N - 1, row[i][N - 1], false);
-			if (prev != result)printf("row %d back\n", i);
+}
+void ch_init()
+{
+	nfor(i,0,N)ch[i] = false;
+}
+
+//하나의 라인에서 가능성 찾기
+bool calc_line(vector<int>&line,int now)
+{
+	if(now == N-1)return true;
+	else{
+		if(line[now] == line[now+1]){
+			return calc_line(line,now+1);
 		}
-		prev = result;
-		search_line(col[i], 0, col[i][0], false);
-		if (prev != result)printf("col %d prev\n", i);
-		if (prev == result) {
-			search_line_reverse(col[i], N - 1, col[i][N - 1], false);
-			if (prev != result)printf("col %d back\n", i);
+		else if(now+L < N && line[now] - line[now+1] == 1)
+		{
+			bool flag = true;
+			for(int i = 2; i <= L;++i){
+				if(line[now+i] != line[now+1]){
+					flag = false;
+					break;
+				}
+			}
+			if(flag){
+				for(int i = now+1; i <= now+L;++i){
+					ch[i] = true;
+				}
+				return calc_line(line,now+L);
+			}
 		}
+		else if(now-L+1 >= 0 && line[now] - line[now+1] == -1 && !ch[now]){
+			bool flag = true;
+			for(int i = 1; i <= L-1;++i){
+				if(line[now-i] != line[now] || ch[now-i]){
+					flag = false;
+					break;
+				}
+			}
+			if(flag)return calc_line(line,now+1);
+		}
+		return false;
 	}
-	cout << result;
+}
+
+int main()
+{
+	init();
+	int res = 0;
+	nfor(i,0,N){
+	ch_init();
+		if(calc_line(col[i],0))res++;
+	ch_init();
+		if(calc_line(raw[i],0))res++;
+	}
+	cout<<res<<'\n';
 	return 0;
 }
