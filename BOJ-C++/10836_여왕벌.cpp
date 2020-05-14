657831d064ad358259e744dab3ee5e6b4ddba259
gamjagoon <rlaalswo201@gmail.com>
2020-05-15 01:12:58 +0900

    fast I/O

--- BOJ-C++/10836_여왕벌.cpp
+++ BOJ-C++/10836_여왕벌.cpp
@@ -4,21 +4,40 @@ version : gcc 9.2.0 c++17
 problem : https://www.acmicpc.net/problem/10836
 summary : 시뮬 구현
 */
-#include <iostream>
-#define endl '\n'
-using namespace std;
+#include <cstdio>
 int M,N;
 int arr[1500];
 
+char in[1<<25]; // sizeof in varied in problem
+char const*o;
+
+void init_in()
+{ 
+    o = in; 
+    in[ fread(in,1,sizeof(in)-4,stdin)]=0;//set 0 at the end of buffer.
+}
+
+int readInt(){ 
+   unsigned u = 0, s = 0;
+   
+   while(*o <= 32)++o; //skip whitespaces... 
+   
+   while(*o>='0' && *o<='9')u = (u<<3) + (u << 1) + (*o++ - '0');
+   
+   return static_cast<int>( (u^s) + s) ;
+}
 
 //Global
 int main() {
-  ios::sync_with_stdio(false);cin.tie(0);
-  cin>>M>>N;
+  init_in();
+  M = readInt();
+  N = readInt();
   int len = M*2 -1;
   int a,b,c;
   while(N--){
-    cin>>a>>b>>c;
+    a = readInt();
+    b = readInt();
+    c = readInt();
     arr[a]++;
     arr[a+b]++;
   }
@@ -28,10 +47,10 @@ int main() {
   }
   arr[len-1]++;
   for(int r = 0; r < M; r++){
-    cout<<arr[len - M - r]<<' ';
+    printf("%d ",arr[len - M - r]);
     for(int c =M ; c < len; c++){
-        cout<<arr[c]<<' ';
+        printf("%d ",arr[c]);
     }
-    cout<<endl;
+    printf("\n");
   }
 }
