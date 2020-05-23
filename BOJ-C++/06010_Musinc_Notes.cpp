a4705273e3665619a4ad8a4a5efcbd35d583372f
gamjagoon <rlaalswo201@gmail.com>
2020-05-23 16:12:34 +0900

    using map

--- BOJ-C++/06010_Musinc_Notes.cpp
+++ BOJ-C++/06010_Musinc_Notes.cpp
@@ -1,15 +1,32 @@
 /*
-Date : 05/21/2020
+Date : 05/23/2020
 version : gcc 9.2.0 c++17
 problem : 
-summary : 
+summary : count
 */
 #include <iostream>
-
+#include <map>
 #define endl '\n'
-#define Loop(i,s,e) for(auto i = s; i < e; ++i)
-#define io ios::sync_with_stdio(false);cin.tie(0);
-
 using namespace std;
-
+typedef long long ll;
 //Global
+ll N,Q;
+map<ll,ll>T;
+
+int main() {
+  ios::sync_with_stdio(false);cin.tie(0);
+  cin>>N>>Q;
+  ll tmp;
+  long long s = 0;
+  T.insert({s,1});
+  for(ll i = 1; i <= N; i++){
+    cin>>tmp;
+    s += tmp;
+    T.insert({s,i+1});
+  }
+  long long query;
+  for(ll i = 0; i < Q; i++){
+    cin>>query;
+    cout<<(*T.upper_bound(query)).second-1<<endl;;
+  }
+}
